da560744b0fb7b08e4ff08eb622f49ab1816e757
Ethan James <ethanjamesauto@vt.edu>
2023-05-28 12:36:24 -0700

    wip - add DAW data structures

---
+++ pico/src/player.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <vector>
+
+class Clip {
+public:
+    void* data;         // pointer to audio data
+    uint size;          // in samples
+    uint timestamp;     // in samples
+};
+
+class Track {
+public:
+    std::vector<Clip> clips;
+};
+
+class Player {
+public:
+    uint head_pos;
+    std::vector<Track> tracks;
+};
