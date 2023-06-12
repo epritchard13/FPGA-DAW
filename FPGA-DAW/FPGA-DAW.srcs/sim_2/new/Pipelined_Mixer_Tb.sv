f96ab46f956033b6ecf710005f68ffbe19ada3b0
epritchard13 <89549127+epritchard13@users.noreply.github.com>
2023-06-12 18:27:05 -0400

    working

--- FPGA-DAW/FPGA-DAW.srcs/sim_2/new/Pipelined_Mixer_Tb.sv
+++ FPGA-DAW/FPGA-DAW.srcs/sim_2/new/Pipelined_Mixer_Tb.sv
@@ -1,26 +1,46 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06/12/2023 06:06:06 PM
-// Design Name: 
-// Module Name: Pipelined_Mixer_Tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+module Pipelined_Mixer_Tb();
+    logic clk;
+    logic resetn;
+    logic control;
+    logic dsp_axi_data;
+    logic dsp_axi_valid;
+    logic dsp_axi_ready;
+    logic mix_axi_data;
+    logic mix_axi_valid;
+    logic mix_axi_ready;
+    
+    Pipelined_Mixer plm(.clk(clk),
+                        .resetn(resetn),
+                        .control(control),
+                        .dsp_axi_data(dsp_axi_data),
+                        .dsp_axi_valid(dsp_axi_valid),
+                        .dsp_axi_ready(dsp_axi_ready),
+                        .mix_axi_data(mix_axi_data),
+                        .mix_axi_valid(mix_axi_valid),
+                        .mix_axi_ready(mix_axi_ready));
+                        
+    initial begin
+        clk = 0;
+        forever begin
+            clk = ~clk;
+            #20;
+        end
+    end
+    initial begin
+        resetn = 0;
+        control = 8;
+        dsp_axi_data = 5555;
+        dsp_axi_valid = 0;
+        mix_axi_ready = 0;
+        #10;
+        resetn = 1;
+        #10;
+        dsp_axi_valid = 1;
+        mix_axi_ready = 1;
+        #200;
+        $stop;
+    end
 
-module Pipelined_Mixer_Tb(
-
-    );
 endmodule
--- FPGA-DAW/FPGA-DAW.srcs/sources_1/new/Pipelined_Mixer.sv
+++ FPGA-DAW/FPGA-DAW.srcs/sources_1/new/Pipelined_Mixer.sv
@@ -14,12 +14,12 @@ module Pipelined_Mixer#(
     //axi interface from DSP CORE
     input [SAM_WID-1:0] dsp_axi_data,
     input dsp_axi_valid,
-    output dsp_axi_ready,
+    output dsp_axi_ready,           //held to 1;
     
     //axi interface out of mixer
     output [SAM_WID-1:0] mix_axi_data,
     output mix_axi_valid,
-    input mix_axi_ready
+    input mix_axi_ready             //at the moment this module is the master. this is disconnected.
     );
     
 reg [2*(SAM_WID-1)-1:0] atten_sam;      //The attenuated sample does not lose precision.
@@ -53,24 +53,40 @@ logic read_ptr;
 logic write_ptr;
 reg [2*(SAM_WID-1)-1:0] delay;
 
+logic buffer_can_read;
+assign buffer_can_read = read_ptr == write_ptr;
+
+
 always @(posedge clk) begin
     if(resetn == 0)begin
-        read_ptr <= 0;
+        read_ptr    <= 0;
+        delay       <= 0;
+    end else begin
+        //read value to be accumulated to when not writing.
+        if(buffer_can_read)begin
+            delay <= acc_buff[read_ptr] + atten_sam;
+            read_ptr <= read_ptr + 1;
+        end
+    end
+end
+
+always @(posedge clk) begin
+    if(resetn == 0)begin
         write_ptr <= 0;
         for(int i=0; i<NUM_TRA; i=i+1)begin
             acc_buff[i] <= 0;
         end
     end else begin
         if(dsp_axi_valid)begin
-            //read value to be accumulated to
-            delay <= acc_buff[read_ptr] + atten_sam;
-            read_ptr <= read_ptr + 1;
-        
-            //write sum back to buffer
+            //write sum back to buffer when dsp output is valid
             acc_buff[write_ptr] <= delay;
             write_ptr <= write_ptr + 1;
         end
     end
 end
+
+assign dsp_axi_ready = 1;
+assign mix_axi_data = delay;
+assign mix_axi_valid = buffer_can_read;
 
 endmodule
