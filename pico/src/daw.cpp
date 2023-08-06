a488a44b82c9e5a691bc61ad43efc76f35500c70
Ethan James <ethanjamesauto@vt.edu>
2023-08-05 19:22:09 -0700

    clean up code

--- pico/src/daw.cpp
+++ pico/src/daw.cpp
@@ -2,19 +2,17 @@
 #include "pico/stdlib.h"
 #include "spi_link.h"
 #include <cmath>
-#include "player.h"
+#include "daw/player.h"
 #include "stdio_mem.h"
 #include "mmc/sdc_example.h"
 #include <iostream>
 #include <cstring>
 #include <stdio.h>
 
-Player test_player;
-//uint8_t buf[1024];
-uint audio_size;
-
 #include "fatfs/ff.h"
 #include "fatfs/diskio.h"
+
+Player test_player;
 
 int fat_example_main(void);
 int fat_test_write(void);
@@ -104,16 +102,6 @@ void read_stdin()
             printf("unknown command: %s\n", cmd);
         }
     }
-    /*else if (c == 0x53) {
-        fread(&audio_size, 1, 4, stdin);
-        if (audio_size <= sizeof(buf)) {
-            fread(buf, 1, audio_size, stdin);
-            uint16_t val = audio_size - 1;
-            uint8_t cmd[] = { 0x88, (uint8_t) (val & 0xff), (uint8_t) ((val >> 8) & 0xff)};
-            spi_write_blocking(SPI_PORT, cmd, sizeof(cmd));
-            spi_write_blocking(SPI_PORT, buf, audio_size); 
-        }
-    }*/
 }
 
 int main()
@@ -125,15 +113,8 @@ int main()
 
     fread(NULL, 1, 1, stdin); // don't start any of the code until we get a single byte
 
-    audio_size = 1024*2;
     while (true) {
         read_stdin();
-        /*read_blocking(buf, 0, audio_size);
-        write_blocking(buf, 0, audio_size);
-        uint16_t val = audio_size - 1;
-        uint8_t cmd[] = { 0x88, (uint8_t) (val & 0xff), (uint8_t) ((val >> 8) & 0xff)};
-        spi_write_blocking(SPI_PORT, cmd, sizeof(cmd));
-        spi_write_blocking(SPI_PORT, buf, audio_size); //*/
     }
 
     return 0;
